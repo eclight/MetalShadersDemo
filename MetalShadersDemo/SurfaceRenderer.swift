7018fa28f4c22fded6cf435ff85111aa3be6e9d4
eclight <eclight@users.noreply.github.com>
2020-03-03 10:42:53 +0300

    Code formatting

--- MetalShadersDemo/SurfaceRenderer.swift
+++ MetalShadersDemo/SurfaceRenderer.swift
@@ -24,75 +24,75 @@ class SurfaceRenderer {
     private let commandQueue: MTLCommandQueue
     private let threadsPerThreadgroup: MTLSize
     private let threadGroupsPerGrid: MTLSize
-    
+
     private let vertexData: [Float] = [
         -1.0, -1.0, 0.0, 0.0,
-        -1.0,  1.0, 0.0, 1.0,
+        -1.0, 1.0, 0.0, 1.0,
         1.0, -1.0, 1.0, 0.0,
-        
-        -1.0,  1.0, 0.0, 1.0,
-        1.0,  1.0, 1.0, 1.0,
-        1.0, -1.0, 1.0, 0.0
+
+        -1.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, 1.0, 1.0,
+        1.0, -1.0, 1.0, 0.0,
     ]
-    
+
     init(device: MTLDevice, backgroundImage: CGImage, gridSize: (Int, Int)) {
         self.device = device
-        
+
         let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
         vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])!
-        
+
         let defaultLibrary = device.makeDefaultLibrary()!
         let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
         let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
-        
+
         dropBuffer = device.makeBuffer(length: MemoryLayout<Drop>.size)!
-        
+
         let gridTextureDescriptor = MTLTextureDescriptor()
         let (width, height) = gridSize
         gridTextureDescriptor.pixelFormat = .rg32Float
         gridTextureDescriptor.width = width
         gridTextureDescriptor.height = height
         gridTextureDescriptor.usage = [.shaderWrite, .shaderRead]
-        
+
         backHeightMap = device.makeTexture(descriptor: gridTextureDescriptor)!
         frontHeightMap = device.makeTexture(descriptor: gridTextureDescriptor)!
         normalMap = device.makeTexture(descriptor: gridTextureDescriptor)!
-        
+
         let loader = MTKTextureLoader(device: device)
         backgroundMap = try! loader.newTexture(cgImage: backgroundImage, options: [MTKTextureLoader.Option.SRGB: false])
-        
+
         let updateHeightmapFunction = defaultLibrary.makeFunction(name: "update_heightmap")
         updateHeightmapPipelineState = try! device.makeComputePipelineState(function: updateHeightmapFunction!)
-        
+
         let addDropFunction = defaultLibrary.makeFunction(name: "add_drop")
         addDropPipleineState = try! device.makeComputePipelineState(function: addDropFunction!)
-        
+
         let computeNormalsFunction = defaultLibrary.makeFunction(name: "compute_normals")
         computeNormalsPipelineState = try! device.makeComputePipelineState(function: computeNormalsFunction!)
-        
+
         let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
         pipelineStateDescriptor.vertexFunction = vertexProgram
         pipelineStateDescriptor.fragmentFunction = fragmentProgram
         pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        
+
         renderPipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
-        
+
         commandQueue = device.makeCommandQueue()!
-        
+
         let w = updateHeightmapPipelineState.threadExecutionWidth
         let h = updateHeightmapPipelineState.maxTotalThreadsPerThreadgroup / w
         threadsPerThreadgroup = MTLSizeMake(w, h, 1)
         threadGroupsPerGrid = MTLSizeMake((frontHeightMap.width + w - 1) / w,
                                           (frontHeightMap.height + h - 1) / h, 1)
     }
-    
+
     func render(drawable: CAMetalDrawable, drops: [Drop]) {
         let renderPassDescriptor = MTLRenderPassDescriptor()
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
-        
+
         let commandBuffer = commandQueue.makeCommandBuffer()!
         let computeEncoder = commandBuffer.makeComputeCommandEncoder()!
-        
+
         for drop in drops {
             computeEncoder.setComputePipelineState(addDropPipleineState)
             computeEncoder.setTexture(frontHeightMap, index: 0)
@@ -100,41 +100,41 @@ class SurfaceRenderer {
             computeEncoder.setBuffer(dropBuffer, offset: 0, index: 0)
             writeDropData(drop: drop, buffer: dropBuffer)
             computeEncoder.dispatchThreadgroups(threadGroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
-            
+
             swap(&frontHeightMap, &backHeightMap)
         }
 
         computeEncoder.setComputePipelineState(updateHeightmapPipelineState)
-        
-        for _ in 0..<3 {
+
+        for _ in 0 ..< 3 {
             computeEncoder.setTexture(frontHeightMap, index: 0)
             computeEncoder.setTexture(backHeightMap, index: 1)
             computeEncoder.dispatchThreadgroups(threadGroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
-            
+
             swap(&frontHeightMap, &backHeightMap)
         }
-        
+
         computeEncoder.setComputePipelineState(computeNormalsPipelineState)
         computeEncoder.setTexture(frontHeightMap, index: 0)
         computeEncoder.setTexture(normalMap, index: 1)
         computeEncoder.dispatchThreadgroups(threadGroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
-        
+
         computeEncoder.endEncoding()
-        
+
         let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
         renderEncoder.setRenderPipelineState(renderPipelineState)
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        
+
         renderEncoder.setFragmentTexture(normalMap, index: 0)
         renderEncoder.setFragmentTexture(backgroundMap, index: 1)
-        
+
         renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 6, instanceCount: 1)
         renderEncoder.endEncoding()
-        
+
         commandBuffer.present(drawable)
         commandBuffer.commit()
     }
-    
+
     private func writeDropData(drop: Drop, buffer: MTLBuffer) {
         let bufferDataPointer = buffer.contents()
         bufferDataPointer.storeBytes(of: drop, as: Drop.self)
--- MetalShadersDemo/ViewController.swift
+++ MetalShadersDemo/ViewController.swift
@@ -1,26 +1,26 @@
 
-import UIKit
 import Metal
 import MetalKit
+import UIKit
 
 class ViewController: UIViewController {
     var metalLayer: CAMetalLayer!
     var renderer: SurfaceRenderer!
     var timer: CADisplayLink!
     var drops: [Drop] = []
-    
+
     var tapRecognizer: UITapGestureRecognizer!
     var dragRecognizer: UIPanGestureRecognizer!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         dragRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleTap))
-        
+
         view.addGestureRecognizer(tapRecognizer)
         view.addGestureRecognizer(dragRecognizer)
-        
+
         let device = MTLCreateSystemDefaultDevice()!
         metalLayer = CAMetalLayer()
         metalLayer.device = device
@@ -28,32 +28,32 @@ class ViewController: UIViewController {
         metalLayer.framebufferOnly = true
         metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
-        
+
         let backgroundImage = UIImage(named: "Background")!
         let w = metalLayer.bounds.width * metalLayer.contentsScale * 3
         let h = metalLayer.bounds.height * metalLayer.contentsScale * 3
         let croppedBackgroundImage = backgroundImage.cgImage!.cropping(to: CGRect(x: 0, y: 0, width: w, height: h))!
-        
-        let aspect = Float(view.frame.height / view.frame.width);
+
+        let aspect = Float(view.frame.height / view.frame.width)
         let height: Int = 256
         renderer = SurfaceRenderer(device: device, backgroundImage: croppedBackgroundImage, gridSize: (Int(Float(height) / aspect + 0.5), height))
-        
+
         timer = CADisplayLink(target: self, selector: #selector(drawFrame))
         timer.add(to: RunLoop.main, forMode: .default)
     }
-    
+
     func render() {
         guard let drawable = metalLayer?.nextDrawable() else { return }
         renderer.render(drawable: drawable, drops: drops)
         drops.removeAll()
     }
-    
+
     @objc func drawFrame() {
         autoreleasepool {
             render()
         }
     }
-    
+
     @objc
     func handleTap(sender: UITapGestureRecognizer) {
         if sender.state == .ended || sender.state == .changed {
@@ -62,10 +62,10 @@ class ViewController: UIViewController {
                 x: Float(location.x / view.frame.width),
                 y: Float(view.frame.height / view.frame.width) - Float(location.y / view.frame.width),
                 radius: 0.04,
-                strength: 0.0009)
-            
+                strength: 0.0009
+            )
+
             drops.append(drop)
         }
     }
 }
-
